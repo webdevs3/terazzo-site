546debd3f19450f2df9a2293d3af57410634edd4
webdevs3 <117022768+webdevs3@users.noreply.github.com>
2022-10-30 19:30:57 +0530

    Delete Terazzo.js

--- Terazzo.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('ul');
-
-hamburger.addEventListener('click',()=>{
-    navbar.classList.toggle('slide');
-});
