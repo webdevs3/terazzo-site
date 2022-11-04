1dc25bef48aa11bb98e71ae644bf2c2465114635
webdevs3 <117022768+webdevs3@users.noreply.github.com>
2022-11-04 12:01:50 +0530

    Add files via upload

---
+++ home-pages/js/terazzo1.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('ul');
+
+hamburger.addEventListener('click',()=>{
+    navbar.classList.toggle('slide');
+});
+
+
+// products duh 
+let headClass = document.getElementsByClassName("prod-img1");
+
+    
